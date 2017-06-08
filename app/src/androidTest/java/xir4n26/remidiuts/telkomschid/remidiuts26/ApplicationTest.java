91c26967bd660deaf1434c02bd9802a7227fd914
ASUS <naufalfakhrian9@gmail.com>
2017-06-08 15:20:12 +0700

    Initial Commit

---
+++ app/src/androidTest/java/xir4n26/remidiuts/telkomschid/remidiuts26/ApplicationTest.java
@@ -0,0 +1,13 @@
+package xir4n26.remidiuts.telkomschid.remidiuts26;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
